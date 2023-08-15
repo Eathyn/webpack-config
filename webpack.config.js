88674c318b987d820da7c950455a05b911e54e93
Eathyn <eathynzakas@gmail.com>
2023-08-15 18:10:13 +0800

    image: compress image
    1.imagemin: lossless optimization

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ const { resolve } = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
 
 const devMode = process.env.NODE_ENV === 'development'
 
@@ -82,6 +83,41 @@ module.exports = {
     minimizer: [
       // 使用 cssnano 优化和压缩 CSS。默认只有在 production 模式下才运行
       new CssMinimizerPlugin(),
+      // 压缩图片
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              [
+                "svgo",
+                {
+                  plugins: [
+                    {
+                      name: "preset-default",
+                      params: {
+                        overrides: {
+                          removeViewBox: false,
+                          addAttributesToSVGElement: {
+                            params: {
+                              attributes: [
+                                { xmlns: "http://www.w3.org/2000/svg" },
+                              ],
+                            },
+                          },
+                        },
+                      },
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+      }),
     ],
   },
 
